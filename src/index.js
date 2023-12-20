85157231063b843414e2badc3e14e20a4bb46053
ErAz7 <webacc1998@gmail.com>
2023-12-20 10:43:16 +0000

    test

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-
+const hasPush = ('PushManager' in window);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
